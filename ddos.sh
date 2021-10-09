fb35b0125693f745cbd7387d1d04362f299b9434
awknode <64115053+awknode@users.noreply.github.com>
2021-10-09 12:22:50 -0700

    creating ddos.sh
    tcpdump .pcap files to /tmp

---
+++ ddos.sh
@@ -0,0 +1,15 @@
+interface=eth0
+ dumpdir=/tmp/
+ while /bin/true; do
+ pkt_old=`grep $interface: /proc/net/dev | cut -d : -f2 | awk '{ print $2 }'`
+ sleep 1
+ pkt_new=`grep $interface: /proc/net/dev | cut -d : -f2 | awk '{ print $2 }'`
+ pkt=$(( $pkt_new-$pkt_old ))
+ echo -ne "\r$pkt packets/s\033[0K"
+ if [ $pkt -gt 10000 ]; then
+ echo -e "\n`date` Under attack, dumping packets." | /bin/mail -s 'DDOS ATTACK XEON' your@emailhere.com
+tcpdump -n -s0 -c 2000 -w $dumpdir/dump.`date +"%Y%m%d-%H%M%S"`.cap
+echo "`date` Packets dumped, sleeping now."
+sleep 300
+fi
+done
